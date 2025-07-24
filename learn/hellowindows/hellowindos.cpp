48375a0747782c5b1b392151663605aab8b3f665
yexiaofeng <yexiaofeng@qq.com>
2025-07-24 14:46:15 +0800

    LearnOpenGL

---
+++ learn/hellowindows/hellowindos.cpp
@@ -0,0 +1,50 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress))
+    {
+         std::cout << "Failed to initialize GLAD" << std::endl;
+         return -1;
+    }
+
+    // 渲染循环
+    while(!glfwWindowShouldClose(window))
+    {
+         // 输入
+        processInput(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+       
+        // 检查并调用事件，交换缓冲
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
